7d47fff657fe9cf52fd62633008542bf7a9b0bd7
j_man <woaksths@ajou.ac.kr>
2017-11-27 21:49:56 +0900

    angular component 구성 및 html

---
+++ src/app/waitingRoom/wait.roomList-component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+
+
+@Component({
+  selector : 'app-wait-room-list',
+  templateUrl: './wait.roomList-component.html',
+  styleUrls: ['./wait.roomList-component.css']
+})
+
+export class WaitRoomListComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+}
