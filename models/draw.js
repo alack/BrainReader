6fdc40abb6bb7c54b1c6ddd237a731d991a61994
Crosstreet74 <crosstreet74@gmail.com>
2017-12-04 19:18:29 +0900

    change draw schema

--- models/draw.js
+++ models/draw.js
@@ -6,38 +6,7 @@ const drawSchema = new Schema({
   created: Date,
   category: String,
   name: String,
-  image: Buffer,
-  time_stamp: [Number],
-  xs: [Number],
-  ys: [Number]
-});
-
-drawSchema.static({
-  createCollection: function (in_times, in_xs, in_ys, callback) {
-    var collection = [];
-    var i = 0;
-    var draw_time = sizeof(in_times);
-    this.time_stamp = new Number[draw_time];
-    this.xs = new Number[draw_time];
-    this.ys = new Number[draw_time];
-
-    in_times.forEach(function (item) {
-      for(i = 0; i<draw_time; i++){
-        this.time_stamp[i] = item;
-      }
-    });
-    in_xs.forEach(function (item) {
-      for(i = 0; i<draw_time; i++){
-        this.x[i] = item;
-      }
-    });
-    in_ys.forEach(function (item) {
-      for(i = 0; i<draw_time; i++){
-        this.ys[i] = item;
-      }
-    });
-    callback(null, collection);
-  }
+  image: Buffer
 });
 
 module.exports = mongoose.model('draw', drawSchema);
