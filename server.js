edcd7bf52336da718104ee0522a1fa86881032b0
Yoo Jaehong <hibria21@gmail.com>
2017-12-07 21:26:23 +0900

    fix body-parser deprecated error @ server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect('mongodb://localhost/web-system-design', { useMongoClient: true
 
 // POST 데이터 파싱 설정
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 // ng build 명령 실행시 생성되는 static 리소스 폴더 경로 및 이름 설정
 app.use(express.static(path.join(__dirname, 'dist')));
 // session 설정
